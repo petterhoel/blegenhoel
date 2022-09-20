ef4ff5323cc00e0ad6fd4dd711db93960c2b1a15
Petter <petter@petterhoel.net>
2022-09-20 18:57:53 +0200

    Some messing around witn images

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,4 +1,13 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
+import sanity from 'astro-sanity'
+
+const sanityConfig = {
+    projectId: '2uvu00qu',
+    dataset: 'staging',
+    useCdn: true,
+}
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    integrations: [sanity(sanityConfig)],
+})
