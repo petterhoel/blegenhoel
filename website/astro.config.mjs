6a19847c8e8ddefe336690709187e955440bffad
Petter <petter@petterhoel.net>
2022-09-23 20:14:58 +0200

    Adds Netlify deploy options

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config'
 import sanity from 'astro-sanity'
+import netlify from "@astrojs/netlify/functions";
 
 const sanityConfig = {
     projectId: '2uvu00qu',
@@ -10,4 +11,6 @@ const sanityConfig = {
 // https://astro.build/config
 export default defineConfig({
     integrations: [sanity(sanityConfig)],
+    output: "server",
+    adapter: netlify()
 })
