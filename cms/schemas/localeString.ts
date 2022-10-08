e57309575ec29fc067e6fb77b6ce012778f16f36
Petter <petter@petterhoel.net>
2022-10-08 13:52:22 +0200

    Adds localeString

---
+++ cms/schemas/localeString.ts
@@ -0,0 +1,17 @@
+export const localeString = {
+  title: 'Localized string',
+  name: 'localeString',
+  type: 'object',
+  fields: [
+    {
+      title: 'Norsk',
+      name: 'no',
+      type: 'string',
+    },
+    {
+      title: 'Engelsk',
+      name: 'en',
+      type: 'string',
+    },
+  ],
+}
