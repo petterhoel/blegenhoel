208a8e697da415cd83dee0dde14337f17f4c8583
Petter <petter@petterhoel.net>
2022-12-04 14:56:27 +0100

    Adds image extraction function and test and jest

--- site/src/cms-integration/image-util.ts
+++ site/src/cms-integration/image-util.ts
@@ -9,3 +9,22 @@ const imageUrlBuilder = sanityImageUrlBuilder({ projectId, dataset })
 export function urlForImage(source: SanityImageSource) {
   return imageUrlBuilder.image(source)
 }
+
+export function extractWithHeughtFromUrl(url: string): ImageDimmentions {
+  const splitPoint = url.lastIndexOf('-')
+  const splitEnd = url.lastIndexOf('.')
+
+  const dimmentionParts = url.substring(splitPoint + 1, splitEnd)
+
+  const xDivider = dimmentionParts.indexOf('x')
+  const width = +dimmentionParts.substring(0, xDivider)
+
+  const height = +dimmentionParts.substring(xDivider + 1)
+
+  return { width, height }
+}
+
+interface ImageDimmentions {
+  width: number
+  height: number
+}
---
+++ site/src/cms-integration/image.util.spec.ts
@@ -0,0 +1,41 @@
+import { extractWithHeughtFromUrl } from './image-util'
+
+describe('image-util', () => {
+  test('extract 300x400', () => {
+    const url = '22222-300x450.jpg'
+    const { width, height } = extractWithHeughtFromUrl(url)
+    expect(width).toBe(300)
+    expect(height).toBe(450)
+  })
+
+  test('extract 2281x3082', () => {
+    const url =
+      'https://cdn.sanity.io/images/2uvu00qu/staging/005774a5e6d728ed950ac7786d397220bcd0fbf6-2281x3082.jpg?w=600'
+    const { width, height } = extractWithHeughtFromUrl(url)
+    expect(width).toBe(2281)
+    expect(height).toBe(3082)
+  })
+
+  test('extract 3333x4444', () => {
+    const url =
+      'https://cdn.sanity.io/images/2uvu00qu/staging/005774a5e6d728ed950ac7786d397220bcd0fbf6-4444x3333.jpg?w=600'
+    const { width, height } = extractWithHeughtFromUrl(url)
+    expect(width).toBe(4444)
+    expect(height).toBe(3333)
+  })
+
+  test('extract 2x3', () => {
+    const url =
+      'https://cdn.sanity.io/images/2uvu00qu/staging/005774a5e6d728ed950ac7786d397220bcd0fbf6-2x3.jpg?w=600'
+    const { width, height } = extractWithHeughtFromUrl(url)
+    expect(width).toBe(2)
+    expect(height).toBe(3)
+  })
+
+  test('return zeroes as default', () => {
+    const url = 'aldkjfladjf'
+    const { width, height } = extractWithHeughtFromUrl(url)
+    expect(width).toBe(0)
+    expect(height).toBe(0)
+  })
+})
