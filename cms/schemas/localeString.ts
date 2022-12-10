77b772d45d727c4145aea6f1f6be1defe8e1e9a7
Petter <petter@petterhoel.net>
2022-12-10 10:11:57 +0100

    migrate schema localestring

--- cms/schemas/localeString.ts
+++ cms/schemas/localeString.ts
@@ -1,19 +1,20 @@
 import { english, norsk } from "./locales";
+import { defineType, defineField } from 'sanity'
 
-export const localeString = {
+export const localeString = defineType({
   title: 'Localized string',
   name: 'localeString',
   type: 'object',
   fields: [
-    {
+    defineField({
       title: 'Norsk',
       name: norsk,
       type: 'string',
-    },
-    {
+    }),
+    defineField({
       title: 'Engelsk',
       name: english,
       type: 'string',
-    },
+    }),
   ],
-}
+})
