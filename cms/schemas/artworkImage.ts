8ab9e3127390e91c5cb5cbc09b3c8310bb5a32e4
Petter <petter@petterhoel.net>
2022-12-10 09:49:58 +0100

    mirtate schema artworkImage

--- cms/schemas/artworkImage.ts
+++ cms/schemas/artworkImage.ts
@@ -1,17 +1,19 @@
-export const artworkImage = {
+import { defineType, defineField } from 'sanity'
+
+export const artworkImage = defineType({
   name: 'artworkImage',
   type: 'object',
   title: 'Foto av verk',
   fields: [
-    {
+    defineField({
       name: 'image',
       type: 'image',
       title: 'Avfotografert verk',
-    },
-    {
+    }),
+    defineField({
       name: 'alt',
       type: 'localeString',
       title: 'Beskrivende tekst (for blinde)',
-    },
+    }),
   ],
-}
+})
