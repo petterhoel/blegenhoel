ac6b35546ba32dd3a0189f58c1ef563c9262c5ab
Petter <petter@petterhoel.net>
2023-01-15 16:49:49 +0100

    Introduces environment variables.

--- site/src/env.d.ts
+++ site/src/env.d.ts
@@ -1,1 +1,14 @@
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+    readonly SITE_TITLE_SUFFIX: string;
+    readonly SANITY_PROJECT_ID: string;
+    readonly SANITY_DATASET: string;
+    readonly SANITY_TOKEN: string;
+    readonly SANITY_API_VERSION: string;
+    readonly SANITY_USE_CDN: string;
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
