88ea9a650c1fd62817d67da189cdc0c27e33abde
Petter <petter@petterhoel.net>
2023-04-24 19:23:43 +0200

    Fixes header width and uses webp for image format

--- site/src/cms-integration/image-util.ts
+++ site/src/cms-integration/image-util.ts
@@ -10,7 +10,7 @@ export function urlForImage(source: SanityImageSource | undefined) {
   if (!source) {
     return ''
   }
-  return imageUrlBuilder.image(source).width(600).url()
+  return imageUrlBuilder.image(source).width(600).format("webp").quality(70).url()
 }
 
 export function extractWithHeughtFromUrl(url: string): ImageDimmentions {
