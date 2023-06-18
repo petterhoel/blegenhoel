3ad2714088b72d3a4ec5c46626cc13c760526e9b
Petter <petter@petterhoel.net>
2023-06-18 15:43:42 +0200

    false

--- site/src/cms-integration/sanity-config.ts
+++ site/src/cms-integration/sanity-config.ts
@@ -1,7 +1,7 @@
 export const sanityConfig = {
   projectId: '2uvu00qu',
   dataset: import.meta.env.SANITY_DATASET,
-  useCdn: import.meta.env.SANITY_USE_CDN?.toLowerCase() === 'true',
+  useCdn: false,
   apiVersion: '2021-03-25', // use current UTC date - see "specifying API version"!
   token: '',
 }
