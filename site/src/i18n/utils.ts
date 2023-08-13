6b2f8a8f00852d787e2aa4d17027b8309ef07b8b
Petter <petter@petterhoel.net>
2023-08-13 12:28:24 +0200

    languageselector works

--- site/src/i18n/utils.ts
+++ site/src/i18n/utils.ts
@@ -2,8 +2,17 @@ import { defaultLanguage, uiText } from './ui'
 
 export const getLangFromUrl = (url: URL) => {
   const [, lang] = url.pathname.split('/')
-  if (lang in uiText) return lang as keyof typeof uiText;
+  if (lang in uiText) {
+    return lang as keyof typeof uiText;
+  }
   return defaultLanguage;
+}
+
+export const getUrlWithLang = (url: URL) => {
+  if (url.pathname.startsWith(`/${defaultLanguage}`)) {
+    return url.pathname.replace(`/${defaultLanguage}`, `/en`)
+  }
+  return url.pathname.replace(`/en`, `/${defaultLanguage}`)
 }
 
 export const useTranslation = (lang: keyof typeof uiText) => {
