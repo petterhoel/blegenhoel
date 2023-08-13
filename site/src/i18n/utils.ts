7eba9e9fed763dfe01586dae65b6937c86d1d505
Petter <petter@petterhoel.net>
2023-08-13 10:42:45 +0200

    started

---
+++ site/src/i18n/utils.ts
@@ -0,0 +1,15 @@
+import { defaultLanguage, uiText } from './ui'
+
+export const getLangFromUrl = (url: URL) => {
+  const [, lang] = url.pathname.split('/')
+  if (lang in uiText) return lang as keyof typeof uiText;
+  return defaultLanguage;
+}
+
+export const useTranslation = (lang: keyof typeof uiText) => {
+  return function t(key: keyof typeof uiText[typeof defaultLanguage]){
+
+    // @ts-ignore
+    return uiText[lang][key] || uiText[defaultLanguage][key]
+  }
+}
