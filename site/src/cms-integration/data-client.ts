45221a9f405502fd6323ca3bd5fa6203fe72cc3f
Petter <petter@petterhoel.net>
2023-12-03 16:39:56 +0100

    Allows multiple galleries while treating homepage as a special thing until we have transitioned to multiple galleries.

--- site/src/cms-integration/data-client.ts
+++ site/src/cms-integration/data-client.ts
@@ -1,12 +1,4 @@
 import { createClient } from '@sanity/client'
 import { sanityConfig } from './sanity-config'
 
-const { projectId, dataset, apiVersion, useCdn, token } = sanityConfig
-
-export const dataClient = createClient({
-  projectId,
-  dataset,
-  apiVersion,
-  token,
-  useCdn,
-})
+export const dataClient = createClient(sanityConfig);
---
+++ site/src/i18n/i18n-string.dto.ts
@@ -0,0 +1,4 @@
+export interface I18nString {
+  no: string
+  en: string
+}
