5cedf6ef8141213869f9cc8bcff17af421d4f65c
Petter <1263775+petterhoel@users.noreply.github.com>
2023-12-03 16:45:48 +0100

    Allows multiple galleries when fetching homepage (#50)

--- site/src/cms-integration/sanity-config.ts
+++ site/src/cms-integration/sanity-config.ts
@@ -1,7 +1,18 @@
-export const sanityConfig = {
+
+export const sanityConfig: SanityConfig = {
   projectId: '2uvu00qu',
   dataset: import.meta.env.SANITY_DATASET,
-  useCdn: false,
+  useCdn: true,
+  perspective: 'published',
   apiVersion: '2021-03-25', // use current UTC date - see "specifying API version"!
   token: '',
+}
+
+interface SanityConfig {
+  projectId: string,
+  dataset: string,
+  useCdn: boolean,
+  perspective: 'published',
+  apiVersion: string, // use current UTC date - see "specifying API version"!
+  token: string,
 }
