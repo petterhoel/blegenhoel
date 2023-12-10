bd0eb8cdd8c1ad31069281442e36c3f5f659a4c1
Petter <1263775+petterhoel@users.noreply.github.com>
2023-12-10 11:30:13 +0100

    Prøver å fikse permanent redirect

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -3,6 +3,9 @@ import { defineConfig } from 'astro/config'
 // https://astro.build/config
 export default defineConfig({
   redirects: {
-    '/':'/no'
+    '/': {
+      status: 302,
+      destination: '/no'
+    }
   }
 })
