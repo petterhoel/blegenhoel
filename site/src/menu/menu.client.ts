513f6f32b19153072b5da73d73a8aa25bb3e00d7
Petter <petter@petterhoel.net>
2024-07-17 14:04:48 +0200

    yas

--- site/src/menu/menu.client.ts
+++ site/src/menu/menu.client.ts
@@ -1,12 +1,12 @@
 import groq from 'groq'
+import type { MenuQueryResult } from '../cms-integration/cms-types'
 import { dataClient } from '../cms-integration/data.client'
 
-const menuQuery = groq`*[_type == "publishedGalleries"][0] {
+export async function getGalleryMenuItems(): Promise<MenuQueryResult> {
+  const menuQuery = groq`*[_type == "publishedGalleries"][0] {
   menuGalleries[]-> {
     galleryName {no, en},
     'slug' : gallerySlug.current
     }}`
-
-export async function getGalleryMenuItems() {
-  return await dataClient.fetch(menuQuery)
+  return await dataClient.fetch<MenuQueryResult>(menuQuery)
 }
