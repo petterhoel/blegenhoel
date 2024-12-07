2dcf78ab2eeae8ee0135d30cb5ca56cb9c551e73
Petter <1263775+petterhoel@users.noreply.github.com>
2024-12-07 12:16:06 +0100

    Deps, newer viewtrans and homepage image fix (#71)
    * some fixes
    
    * deps and new viewtrans
    
    * Fixes image fit on narrow portraits on home page

--- site/src/artwork/forside.client.ts
+++ site/src/artwork/forside.client.ts
@@ -1,17 +1,5 @@
 import groq from 'groq'
 import { dataClient } from '../cms-integration/data.client'
-import type { ArtworkDto } from './artwork.dto'
-
-export async function getForsideArtworks(): Promise<ArtworkDto[]> {
-  const artworkQuery = groq`*[_type == "gallery" && _id == 'gallery'][0]{galleryImages[]->}`
-  const artworks = await dataClient.fetch<{
-    galleryImages: ArtworkDto[] | undefined
-  }>(artworkQuery)
-  if (artworks?.galleryImages) {
-    return artworks.galleryImages
-  }
-  return []
-}
 
 export async function getForsideGallerier() {
   const forsideGallerierQuery = groq`*[_type == "publishedGalleries"][0] {
